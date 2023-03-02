ae5d96a5102039b3e917eed7547fc1ab72b9986a
JustasGecevicius <justas.gece@gmail.com>
2023-03-02 11:53:45 +0100

    fixed the search area css

--- src/features/searchArea/components/searchMoviesSeries/SearchAreaMoviesSeries.tsx
+++ src/features/searchArea/components/searchMoviesSeries/SearchAreaMoviesSeries.tsx
@@ -1,32 +1,37 @@
 // Components
 import { Header } from "features/header/components/Header";
 import { SearchBarMoviesSeries } from "features/searchArea/components/searchMoviesSeries/SearchBarMoviesSeries";
-import 'react-slideshow-image/dist/styles.css'
+import "react-slideshow-image/dist/styles.css";
 // Styles
 import "features/searchArea/css/searchArea.css";
 import { Fade } from "react-slideshow-image";
 
 export interface Props {
-  links : string[]
+  links: string[];
 }
 
-export const SearchAreaMoviesSeries = ({links} : Props) => {
-
+export const SearchAreaMoviesSeries = ({ links }: Props) => {
   if (!links) {
     return <div>Loading...</div>;
   }
-  
+
   return (
     <div className="slide-container">
-            <Header/>
       <Fade>
         {links.map((imageLink, index) => (
           <div key={index}>
-            <img style={{ width: '100%' }} src={imageLink} alt="backgroundImage"/>
+            <img
+              style={{ width: "100%" }}
+              src={imageLink}
+              alt="backgroundImage"
+            />
           </div>
         ))}
       </Fade>
-      <SearchBarMoviesSeries/>
+      <div className="slideContainerElements">
+        <Header />
+        <SearchBarMoviesSeries />
+      </div>
     </div>
   );
 };
