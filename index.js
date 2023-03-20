2173c7bd3c1efa0d46c9d0485c9d4a529e33cfe3
JustasGecevicius <justas.gece@gmail.com>
2023-03-20 23:24:41 +0100

    Need to find a way to use conditional url parameters, that are not always mandatory

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+require("dotenv").config();
+const generalRoute = require("./routes/generalRoute");
 
 const app = express();
 const port = 8080;
-require("dotenv").config();
 
-const generalRoute = require("./routes/generalRoute");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// app.use("/", (req, res) => {
+//   console.log("default page");
+//   res.send("Server Running (Default page)");
+// })
 
 app.use("/general", generalRoute);
 
--- routes/generalRoute.js
+++ routes/generalRoute.js
@@ -1,43 +1,59 @@
 const express = require("express");
 const moviedbApi = require("../axios");
 const axios = require("axios");
-
+const admin = require("firebase-admin");
+//const {doc, getDoc} = require("firebase-admin/firestore");
 const secondRoute = require("./secondRoute");
 const generalRoute = express.Router();
 
 generalRoute.get("/:userId/:type/:id/:option/:page", async (req, res) => {
   const { type, id, option, page, userId } = req.params;
+  const db = admin.firestore();
 
   const configPromise = moviedbApi.get(
     `/configuration?api_key=${process.env.MOVIEDB_API_KEY}`
   );
 
-  const dataPromise = moviedbApi.get(
-    id === "all"
-      ? `/${type}/${option}?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=${page}`
-      : `/${type}/${id}?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US`
-  );
+  // const dataPromise = moviedbApi.get(
+  //   id === "all"
+  //     ? `/${type}/${option}?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=${page}`
+  //     : `/${type}/${id}?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US`
+  // );
 
-  const axiosPromises = [configPromise, dataPromise];
+  const userLiked = async () => {
+    try{
+      const docRef = doc(db, `${option === "movie" ? "likedMovies" : "likedSeries"}`, `${userId}`)
+      //const likedRef = db.collection(`${option === "movie" ? "likedMovies" : "likedSeries"}/${userId}`);
 
-  const responses = await axios.all(axiosPromises)
-
-  if (id === "all") {
-     const sortedData = responses[1].data.results.map((elem) => {
-      return {
-        imageURL: `${responses[0].data.images.base_url}${responses[0].data.images.poster_sizes[6]}${elem.poster_path}`,
-        title: elem.title,
-        release_date: elem.release_date,
-        id : elem.id,
-        liked : false,
-        rated : false,
-      };
-    });
-    res.json({
-      success: true,
-      data: sortedData
-    })
+      const response = await getDoc(docRef);
+      return response.data()
+      //console.log(response.data());
+    }
+    catch{
+      console.log("error");
+    }
   }
+
+   const axiosPromises = [configPromise, userLiked];
+
+   const responses = await axios.all(axiosPromises);
+
+  // if (id === "all") {
+  //    const sortedData = responses[1].data.results.map((elem) => {
+  //     return {
+  //       imageURL: `${responses[0].data.images.base_url}${responses[0].data.images.poster_sizes[6]}${elem.poster_path}`,
+  //       title: elem.title,
+  //       release_date: elem.release_date,
+  //       id : elem.id,
+  //       liked : false,
+  //       rated : false,
+  //     };
+  //   });
+  //   res.json({
+  //     success: true,
+  //     data: sortedData
+  //   })
+  // }
   // else {
   //   res.json({
   //     success: true,
